8f29de953ad6d23c41931f1ea1914a854c0a8b9a
Arthur Cousseau <arthur.cousseau@me.com>
2023-07-02 20:08:40 +0200

    generate_ui_files now uses pyrcc5 to generate resource files

--- app/ui/generate_ui_files.py
+++ app/ui/generate_ui_files.py
@@ -4,9 +4,12 @@ import subprocess
 ui_folder = "app/ui/"
 
 for file in os.listdir(ui_folder):
+    file_path = os.path.join(ui_folder, file)
     if file.endswith(".ui"):
-        file_path = os.path.join(ui_folder, file)
         output_file = os.path.splitext(file_path)[0] + ".py"
         subprocess.run(["pyuic5", file_path, "-o", output_file])
+    elif file.endswith(".qrc"):
+        output_file = os.path.splitext(file_path)[0] + "_rc.py"
+        subprocess.run(["pyrcc5", file_path, "-o", output_file])
 
 print("UI files converted to Python successfully.")
