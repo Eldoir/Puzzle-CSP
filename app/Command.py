66255fb780e100bcaacb571750a5e736ee8d22eb
Arthur Cousseau <arthur.cousseau@me.com>
2023-07-03 19:36:23 +0200

    Use None instead of NoReturn
    Simpler

--- app/Command.py
+++ app/Command.py
@@ -1,14 +1,14 @@
-from typing import NoReturn, TypeVar, Generic, Callable
+from typing import TypeVar, Generic, Callable
 
 class ICommand:
-    def execute(self, *args) -> NoReturn:
+    def execute(self, *args) -> None:
         raise NotImplementedError()
 
 class Command(ICommand):
     def __init__(self, func: Callable):
         self.__func = func
 
-    def execute(self, *args) -> NoReturn:
+    def execute(self, *args) -> None:
         self.__func(*args)
 
 T = TypeVar('T')  # Type variable for generic type
@@ -16,6 +16,6 @@ class CommandWithResult(ICommand, Generic[T]):
     def __init__(self, func: Callable):
         self.__func = func
     
-    def execute(self, *args) -> NoReturn:
+    def execute(self, *args) -> None:
         self.result = self.__func(*args)
 
